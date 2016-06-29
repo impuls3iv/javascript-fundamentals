ec182de7832b726f77a26f61ce459759149aae5b
Impuls3iv <sammy.unterman@gmail.com>
2016-06-29 16:39:46 -0400

    done arrays portion

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -6,5 +6,7 @@
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
-
-
+var myArr = ['gouda', 'cheddar', 'brie', 'smoked'];
+for (i = 1; i < myArr.length; i++){
+  console.log("My " + i + " choice is " + myArr[i])
+}
